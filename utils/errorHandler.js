3cb31382d84fc712ade5caadb9b8b35424deb45f
ykwizera <ykwizera@alustudent.com>
2025-07-20 20:50:08 +0200

    error handling

---
+++ utils/errorHandler.js
@@ -0,0 +1,21 @@
+module.exports = (err, req, res, next) => {
+  console.error(err.stack);
+
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({
+      error: 'Validation Error',
+      details: err.errors,
+    });
+  }
+
+  if (err.name === 'CastError') {
+    return res.status(400).json({
+      error: 'Invalid ID format',
+    });
+  }
+
+  res.status(500).json({
+    error: 'Internal Server Error',
+    message: err.message,
+  });
+};
