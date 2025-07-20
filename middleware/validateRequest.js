cdd8adcfae08790592434487fce2df7ae11db741
ykwizera <ykwizera@alustudent.com>
2025-07-20 20:09:57 +0200

    request validation middleware to handle input errors

---
+++ middleware/validateRequest.js
@@ -0,0 +1,16 @@
+const { validationResult } = require('express-validator');
+
+const validateRequest = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array().map(err => ({
+                field: err.param,
+                message: err.msg
+            }))
+        });
+    }
+    next();
+};
+
+module.exports = validateRequest;
