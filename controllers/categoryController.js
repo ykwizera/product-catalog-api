7ede22aa64e5290edc3561d86513178ddcd08482
ykwizera <ykwizera@alustudent.com>
2025-07-20 20:41:32 +0200

    improving code formatting

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,54 +1,55 @@
-const Category = require('../models/category'); 
-
-exports.createCategory = async (req, res, next) => {
-    try {
-        const category = new Category(req.body);
-        await category.save();
-        res.status(201).json(category);
-    } catch (error) {
-        next(error);
-    }
-};
+const Category = require('../models/Category');
 
 exports.getCategories = async (req, res, next) => {
-    try {
-        const categories = await Category.find();
-        res.json(categories);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createCategory = async (req, res, next) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.status(201).json(category);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.getCategoryById = async (req, res, next) => {
-    try {
-        const category = await Category.findById(req.params.id);
-        if (!category) return res.status(404).json({ error: 'Category not found' });
-        res.json(category);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) return res.status(404).json({ error: 'Category not found' });
+    res.json(category);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.updateCategory = async (req, res, next) => {
-    try {
-        const category = await Category.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!category) return res.status(404).json({ error: 'Category not found' });
-        res.json(category);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!category) return res.status(404).json({ error: 'Category not found' });
+    res.json(category);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.deleteCategory = async (req, res, next) => {
-    try {
-        const category = await Category.findByIdAndDelete(req.params.id);
-        if (!category) return res.status(404).json({ error: 'Category not found' });
-        res.json({ message: 'Category deleted successfully' });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) return res.status(404).json({ error: 'Category not found' });
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 };
+const swaggerJsdoc = require('swagger-jsdoc');
