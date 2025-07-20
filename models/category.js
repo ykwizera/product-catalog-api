cfac157d85b46e6ada00e98b24ad12c922fa97b3
ykwizera <ykwizera@alustudent.com>
2025-07-20 20:15:25 +0200

    category model

---
+++ models/category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String, default: '' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Category', categorySchema);
